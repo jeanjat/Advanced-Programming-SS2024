8355855885c8785eafbb00e54eecc0d012710529
jeanjat <jeancarlo2797@gmail.com>
2024-06-30 17:21:25 +0200

    Update list2.py
    Add first function for the exercise list2

--- list2.py
+++ list2.py
@@ -12,8 +12,22 @@
 # all adjacent == elements have been reduced to a single element,
 # so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
 # modify the passed in list.
+
+import numpy as np
+
 def remove_adjacent(nums):
   # +++your code here+++
+  nums1=[]  # Empty list
+  for i in range(0, len(nums)+1): # Go through every element of the list
+        try:
+          if nums[i] != nums[i+1]: # Check if current number and the next one are different
+            nums1.append(nums[i]) # Add to the empty list if true
+        except:
+           pass # Exception for when it is overflowed
+  array=np.array(nums[-1:]) # Define previous list as an array
+  scalar=array 
+  if nums1[-1:] != nums[-1:]: # Exception for last digit
+    nums1.append(scalar) # Add it to the list
   return
 
 
